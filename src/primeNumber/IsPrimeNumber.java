6648a63b327fed63a6a003a075a9763d9580536e
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-03 23:04:54 +0700

    create isPrimeNumber.java

---
+++ src/primeNumber/IsPrimeNumber.java
@@ -0,0 +1,29 @@
+package primeNumber;
+
+import java.util.Scanner;
+
+public class IsPrimeNumber {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int number = scanner.nextInt();
+        if(number < 2){
+            System.out.println(number+ " is not a prime");
+        }else {
+            boolean check = true;
+            int i = 2;
+            while(i < number){
+                if(number % i == 0){
+                    check =  false;
+                    break;
+                }
+                i++;
+            }
+            if(check){
+                System.out.println(number + " is a prime");
+            }else {
+                System.out.println(number + " is not a prime");
+            }
+        }
+    }
+}
